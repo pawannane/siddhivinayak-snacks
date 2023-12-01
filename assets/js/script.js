f515f3ecb97ed6104a15af8d2c2b233e8e985411
yashikm <yashikm@axioned.com>
2023-12-02 03:03:34 +0530

    Completed filter functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,51 @@ const hamburger = document.querySelector(".hamburger");
 const navContainer = document.querySelector(".navbar");
 const navLink = document.querySelectorAll(".navbar a");
 const html = document.querySelector("html");
+const navItems = document.querySelectorAll(".filter-list li");
+const imageItems = document.querySelectorAll(".images-list li");
 const header = document.querySelector(".header");
 
-/************************************ Hamburger logic ***********************************************/
-//add active
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navContainer.classList.toggle("active-nav");
-  html.classList.toggle("html-scroll"); // prevent scrolling
+
+window.addEventListener('load', () => {
+  /************************************ Hamburger logic ***********************************************/
+  //add active
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navContainer.classList.toggle("active-nav");
+    html.classList.toggle("html-scroll"); // prevent scrolling
+  })
+
+  //remove active
+  navLink.forEach(e => {
+    e.addEventListener("click", () => {
+      hamburger.classList.remove("active");
+      navContainer.classList.remove("active-nav");
+      html.classList.remove("html-scroll"); // remove prevent scrolling or user can scroll
+    })
+  });
+
+  const filterImage = () => {
+    navItems.forEach((nav) => {
+      nav.addEventListener("click", (e) => {
+        e.preventDefault();
+        const selectedFood = nav.querySelector("a").getAttribute("data-food");
+
+        // Toggle active class on navigation items
+        navItems.forEach((item) => {
+          item.classList.toggle("active-data");
+        });
+
+        // Show/hide images based on selected food type
+        imageItems.forEach((image) => {
+          const foodType = image.getAttribute("data-food-type");
+          image.style.display = (selectedFood === "all" || selectedFood === foodType) ? "block" : "none";
+        });
+      });
+    });
+  };
+
+  filterImage();
 })
 
-//remove active
-navLink.forEach(e => {
-  e.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navContainer.classList.remove("active-nav");
-    html.classList.remove("html-scroll"); // remove prevent scrolling or user can scroll
-  })
-});
+
+
