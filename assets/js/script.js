27d54016786ba6536c8436706ae8fd86b2142a9b
SandeshM <sm03055603@gmail.com>
2023-12-10 23:17:36 +0530

    pushing slider code

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,42 @@ navLink.forEach(e => {
     html.classList.remove("html-scroll"); // remove prevent scrolling or user can scroll
   })
 });
+
+// slider logic start
+
+let slideIndex = 0;
+
+function plusSlide(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let slides = document.querySelectorAll('.item');
+  let testBtns = document.querySelectorAll('.test-btns a');
+
+  if (n >= slides.length) {
+    slideIndex = 0;
+  }
+  if (n < 0) {
+    slideIndex = slides.length - 1;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slides[slideIndex].style.display = 'flex';
+  // slides[slideIndex].style.width = '100%';
+
+  for (let i = 0; i < testBtns.length; i++) {
+    testBtns[i].classList.remove('active');
+  }
+  testBtns[slideIndex].classList.add('active');
+}
+
+showSlides(slideIndex);
+
+// slider logic end
